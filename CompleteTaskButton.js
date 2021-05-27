6d131922aae138260d01895084a8bf9d16a91229
Mark Underwood <mark.t.underwood@protonmail.com>
2021-05-26 17:44:59 -0800

    Remove unused imports

--- CompleteTaskButton.js
+++ CompleteTaskButton.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Button, Image, Text, View } from 'react-native';
+import { Button } from 'react-native';
 
 export function CompleteTaskButton(props) {
     const [isTaskDone, setIsTaskDone] = React.useState(props.completed);
